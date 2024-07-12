fcda1de35f733612a340b3cd35f2082f569baa52
Wang Nan <110763890+theFool-wn@users.noreply.github.com>
2024-07-12 19:17:40 +0800

    Update jsmind-api.js

--- js/jsmind-api.js
+++ js/jsmind-api.js
@@ -11,30 +11,6 @@ class jsMindApi {
         this.url = url || 'https://v1.api.jsmind.online';
     }
 
-    async loadByKey(key) {
-        const response = await fetch(`${this.url}/mindmaps/${key}`);
-        if (!response.ok) {
-            throw new Error(`Can not find the mindmap, key: ${key}`);
-        }
-        const json = await response.json();
-        if (!json.success) {
-            throw new Error(`Can not find the mindmap, key: ${key}`);
-        }
-        return json.data;
-    }
-
-
-    async share(mind) {
-        const response = await fetch(`${this.url}/mindmaps`, {
-            method: "POST",
-            body: JSON.stringify(mind),
-            headers: {
-                "Content-Type": "application/json; charset=UTF-8"
-            }
-        });
-        return await response.json();
-    }
-
     async loadSample(lang) {
         const response = await fetch(`assets/data_intro_${lang}.json`);
         return await response.json();
